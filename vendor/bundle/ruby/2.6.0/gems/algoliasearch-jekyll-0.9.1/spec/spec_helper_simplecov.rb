f70ec31e452cf3aabf1aa478768765bdb9d760ef
Kallee Buchanan <kallee.buchanan@gmail.com>
2020-04-15 04:24:48 +0000

    Deleted 10002 files via CloudCannon

--- vendor/bundle/ruby/2.6.0/gems/algoliasearch-jekyll-0.9.1/spec/spec_helper_simplecov.rb
+++
@@ -1,9 +0,0 @@
-require 'simplecov'
-
-SimpleCov.configure do
-  load_profile 'test_frameworks'
-end
-
-ENV['COVERAGE'] && SimpleCov.start do
-  add_filter '/.rvm/'
-end
--- vendor/bundle/ruby/2.6.0/gems/appraisal-2.1.0/lib/appraisal/version.rb
+++
@@ -1,3 +0,0 @@
-module Appraisal
-  VERSION = "2.1.0".freeze
-end
